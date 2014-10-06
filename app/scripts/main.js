20fba5a6ae3d2cad1be5339689a78fc0e6bbe264
Philip Knape <Knape@Philips-MacBook-Air.local>
2014-10-07 00:37:31 +0200

    moved functions to helper class, added private variables to keep state in helpers, cleaned up a bit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,10 @@
+
+
 (function() {
 
   view.viewSize = new Size(500, 500);
 
   var debug = false;
-
-
-
-
-
 
   var CreateFace = (function () {
 
@@ -22,6 +19,7 @@
 
       this.init();
       this.events();
+      this.onMouseMove();
 
     }
 
@@ -35,19 +33,34 @@
         });
 
         this.leftEye = new Eye({
-          headSegments: this.nose.positions
+          positions: this.nose.positions,
+          type: 'left'
         });
 
         this.rightEye = new Eye({
-          headSegments: this.nose.positions
+          positions: this.nose.positions,
+          type: 'right'
         });
 
       },
 
       events: function() {
+        console.log('calling events')
         function onMouseMove(event) {
+          console.log('calling2')
           console.log(event.point)
         }
+      },
+
+      onMouseMove: function(event) {
+        console.log('calling')
+            // Each time the mouse is moved, set the content of
+            // the point text to describe the position of the mouse:
+            text.content = 'Your position is: ' + event.point.toString();
+        }
+
+      test: function() {
+        console.log('test from face')
       }
     }
 
@@ -56,45 +69,134 @@
   })()
 
   var Helpers = (function() {
+
+    var headCenter = [];
+    var headCornerPoints = {};
+    var originState = {};
+
     function Helpers() {
+      this.test = 'test'
     }
 
-    Helpers.prototype = {
+    Helpers.prototype.addPoints = function(name, numbers) {
 
-      draw: function() {
+      this[name].flatten(numbers);
+      this[name].smooth();
 
-      },
+      return this;
 
-      findCenterfromSegments: function(segments) {
+    }
 
-        var cornerPoints = {
-          left: segments[0].point.x,
-          top: segments[segments.length / 4 * 1].point.y,
-          right: egments[segments.length / 4 * 2].point.x,
-          bottom: segments[segments.length / 4 * 3].point.y,
-        };
+    Helpers.prototype.findCenterfromSegments = function(segments) {
 
-        return {x: cornerPoints.right - cornerPoints.left, y: cornerPoints.bottom - cornerPoints.top}
+      var i = 0;
+      var length;
 
-      },
+      if (segments) {
+        length = segments.length;
+      } else {
+        return;
+      }
 
-      getCenter: function() {
+      var cornerPoints = {
+        left: segments[0].point.x,
+        top: segments[0].point.y,
+        right: segments[0].point.x,
+        bottom: segments[0].point.y
+      };
 
+      for (; i < length; i++) {
 
+        cornerPoints.left = segments[i].point.x < cornerPoints.left ? segments[i].point.x : cornerPoints.left;
+        cornerPoints.top = segments[i].point.y < cornerPoints.top ? segments[i].point.y : cornerPoints.top;
+        cornerPoints.right = segments[i].point.x > cornerPoints.right ? segments[i].point.x : cornerPoints.right;
+        cornerPoints.bottom = segments[i].point.y > cornerPoints.bottom ? segments[i].point.y : cornerPoints.bottom;
 
-      },
+      }
 
-      setWidth: function() {
+      var x = cornerPoints.left + ((cornerPoints.right - cornerPoints.left) / 2)
+      var y = cornerPoints.top + ((cornerPoints.bottom - cornerPoints.top) / 2)
 
-      },
+      headCenter = [x, y];
+      headCornerPoints = cornerPoints;
 
-      setHeight: function() {
-
+      return {
+        positions: cornerPoints,
+        center: [x, y]
       }
 
     }
 
-  })
+    Helpers.prototype.findCenterfromPosition = function(position, type) {
+      var yPosition = (((position.center[1] - position.positions.top) / 2) + position.positions.top)
+      var xPosition;
+
+      if (type === 'left') {
+        xPosition = (((position.center[0] - position.positions.left) / 2) + position.positions.left)
+      } else {
+        xPosition = (position.positions.right - ((position.center[0] - position.positions.left) / 2));
+      }
+
+      return [xPosition, yPosition]
+    }
+
+    Helpers.prototype.alterShape = function(name, moveDistance, original) {
+
+      var i = 0;
+      var segments = this[name].segments;
+      var length = segments.length;
+      var selectRandomFaceForm = this[original] ? this[original].shape : Math.floor((Math.random() * 2));
+      var firstParam = this[original] ? this[original].firstMove  + Math.floor((Math.random() * 3) + 1) : Math.floor((Math.random() * moveDistance) + 1);
+      var secondParam = this[original] ? this[original].secondMove + Math.floor((Math.random() * 3) + 1) : Math.floor((Math.random() * moveDistance) + 1);
+
+      switch(selectRandomFaceForm) {
+        case 0: // wide face aka stewie
+          segments[0].point.x = segments[0].point.x - firstParam;
+          segments[2].point.x = segments[2].point.x + firstParam;
+          segments[3].point.y = segments[3].point.y + secondParam;
+          break;
+        case 1:
+          segments[0].point.x = segments[0].point.x + firstParam;
+          segments[2].point.x = segments[2].point.x - firstParam;
+          segments[3].point.y = segments[3].point.y + secondParam + secondParam;
+        default:
+          break;
+      }
+
+      // create a new shape from the circle
+      // clockwise, starts from left
+
+      this[name].shape = selectRandomFaceForm;
+      this[name].firstMove = firstParam;
+      this[name].secondMove = secondParam;
+
+      return this;
+
+    }
+
+    Helpers.prototype.movePoint = function(name, moveDistance) {
+
+      var i = 0;
+      var segments = this[name].segments;
+      var length = segments.length;
+
+      for (; i < length; i++) {
+
+        var points = segments[i].point
+        points.x = points.x + Math.floor((Math.random() * moveDistance) + 1);
+        points.y = points.y + Math.floor((Math.random() * moveDistance) + 1);
+
+      }
+
+      return this;
+
+    }
+
+    return Helpers;
+
+  })();
+
+
 
   var Head = (function () {
 
@@ -137,83 +239,23 @@
 
     }
 
-    Head.prototype = {
-      createShape: function(head, position, size) {
+    Head.prototype.createShape = function(name, position, size) {
 
-        this[head] = new Path.Circle({
-          center: [position, position],
-          radius: size
-        });
+      var alpha = Math.random()
 
-        this[head].strokeColor = 'black';
-        this[head].strokeWidth = 1;
+      this[name] = new Path.Circle({
+        center: [position, position],
+        radius: size
+      });
 
-        this[head].selected = debug;
+      this[name].strokeColor = new Color(0, 0, 0, alpha);
 
-        return this;
+      this[name].strokeWidth = 1;
 
-      },
+      this[name].selected = debug;
 
-      alterShape: function(head, moveDistance, original) {
+      return this;
 
-        var i = 0;
-        var segments = this[head].segments;
-        var length = segments.length;
-        var selectRandomFaceForm = this[original] ? this[original].shape : Math.floor((Math.random() * 2));
-        var firstParam = this[original] ? this[original].firstMove  + Math.floor((Math.random() * 3) + 1) : Math.floor((Math.random() * moveDistance) + 1);
-        var secondParam = this[original] ? this[original].secondMove + Math.floor((Math.random() * 3) + 1) : Math.floor((Math.random() * moveDistance) + 1);
-
-        switch(selectRandomFaceForm) {
-          case 0: // wide face aka stewie
-            segments[0].point.x = segments[0].point.x - firstParam;
-            segments[2].point.x = segments[2].point.x + firstParam;
-            segments[3].point.y = segments[3].point.y + secondParam;
-            break;
-          case 1:
-            segments[0].point.x = segments[0].point.x + firstParam;
-            segments[2].point.x = segments[2].point.x - firstParam;
-            segments[3].point.y = segments[3].point.y + secondParam + secondParam;
-          default:
-            break;
-        }
-
-        // create a new shape from the circle
-        // clockwise, starts from left
-
-        this[head].shape = selectRandomFaceForm;
-        this[head].firstMove = firstParam;
-        this[head].secondMove = secondParam;
-
-        return this;
-
-      },
-
-      addPoints: function(head, numbers) {
-
-        this[head].flatten(numbers);
-        this[head].smooth();
-
-        return this;
-
-      },
-
-      movePoint: function(head, moveDistance) {
-
-        var i = 0;
-        var segments = this[head].segments;
-        var length = segments.length;
-
-        for (; i < length; i++) {
-
-          var points = segments[i].point
-          points.x = points.x + Math.floor((Math.random() * moveDistance) + 1);
-          points.y = points.y + Math.floor((Math.random() * moveDistance) + 1);
-
-        }
-
-        return this;
-
-      }
     }
 
     return Head;
@@ -240,96 +282,43 @@
 
       this.o = o;
       this.positions = this.findCenterfromSegments(this.o.headSegments)
-      this.nose = {};
 
+      this.nose = null;
       this.createShape('nose', this.positions.center, 10)
           .addPoints('nose', 20)
           .movePoint('nose', 5)
+
+      this.nose2 = null;
+      this.createShape('nose2', this.positions.center, 10)
+          .addPoints('nose2', 20)
+          .movePoint('nose2', 2);
+
+      this.nose3 = null;
+      this.createShape('nose3', this.positions.center, 10)
+          .addPoints('nose3', 20)
+          .movePoint('nose3', 2);
 
       return this;
 
     }
 
-    Nose.prototype = {
-      findCenterfromSegments: function(segments) {
+    Nose.prototype.createShape = function(name, position, size) {
 
-        var i = 0;
-        var length;
+      var alpha = Math.random()
 
-        if (segments) {
-          length = segments.length;
-        } else {
-          return;
-        }
 
-        var cornerPoints = {
-          left: segments[0].point.x,
-          top: segments[0].point.y,
-          right: segments[0].point.x,
-          bottom: segments[0].point.y
-        };
 
-        for (; i < length; i++) {
+      this[name] = new Path.Circle({
+        center: position,
+        radius: size
+      });
 
-          cornerPoints.left = segments[i].point.x < cornerPoints.left ? segments[i].point.x : cornerPoints.left;
-          cornerPoints.top = segments[i].point.y < cornerPoints.top ? segments[i].point.y : cornerPoints.top;
-          cornerPoints.right = segments[i].point.x > cornerPoints.right ? segments[i].point.x : cornerPoints.right;
-          cornerPoints.bottom = segments[i].point.y > cornerPoints.bottom ? segments[i].point.y : cornerPoints.bottom;
+      this[name].strokeColor = new Color(0, 0, 0, alpha);
+      this[name].strokeWidth = 1;
 
-        }
+      // this.head.selected = debug;
 
-        var x = cornerPoints.left + ((cornerPoints.right - cornerPoints.left) / 2)
-        var y = cornerPoints.top + ((cornerPoints.bottom - cornerPoints.top) / 2)
-
-        return {
-          positions: cornerPoints,
-          center: [x, y]
-        }
-
-      },
-
-      createShape: function(noseName, position, size) {
-
-        this[noseName] = new Path.Circle({
-          center: position,
-          radius: size
-        });
-
-        this[noseName].strokeColor = 'black';
-        this[noseName].strokeWidth = 1;
-
-        // this.head.selected = debug;
-
-        return this;
-
-      },
-
-      addPoints: function(head, numbers) {
-
-        this[head].flatten(numbers);
-        this[head].smooth();
-
-        return this;
-
-      },
-
-      movePoint: function(head, moveDistance) {
-
-        var i = 0;
-        var segments = this[head].segments;
-        var length = segments.length;
-
-        for (; i < length; i++) {
-
-          var points = segments[i].point
-          points.x = points.x + Math.floor((Math.random() * moveDistance) + 1);
-          points.y = points.y + Math.floor((Math.random() * moveDistance) + 1);
-
-        }
-
-        return this;
-
-      }
+      return this;
 
     }
 
@@ -355,7 +344,45 @@
 
     function Eye (o) {
       this.o = o;
-      console.log(this)
+      this.center = this.findCenterfromPosition(o.positions, o.type)
+
+      this.eye = null;
+      this.createShape('eye', this.center, 10)
+      .alterShape('eye', 10)
+      .addPoints('eye', 20)
+      .movePoint('eye', 5);
+
+      this.eye2 = null;
+      this.createShape('eye2', this.center, 10)
+      .alterShape('eye2', 10, 'eye')
+      .addPoints('eye2', 20)
+      .movePoint('eye2', 2);
+
+      this.eye3 = null;
+      this.createShape('eye3', this.center, 10)
+      .alterShape('eye3', 10, 'eye')
+      .addPoints('eye3', 20)
+      .movePoint('eye3', 2);
+
+      return this.eye
+
+    }
+
+    Eye.prototype.createShape = function(name, position, size) {
+
+      var alpha = Math.random()
+
+      this[name] = new Path.Circle({
+        center: position,
+        radius: size
+      });
+
+      this[name].strokeColor = new Color(0, 0, 0, alpha);
+      this[name].strokeWidth = 1;
+
+      this[name].selected = debug;
+
+      return this;
     }
 
     return Eye;
@@ -399,9 +426,6 @@
 
   })()
 
-
-
-
   new CreateFace()
 
 
@@ -413,6 +437,7 @@
   text.content = 'Move your mouse over the view, to see its position';
 
 function onMouseMove(event) {
+  console.log('calling')
       // Each time the mouse is moved, set the content of
       // the point text to describe the position of the mouse:
       text.content = 'Your position is: ' + event.point.toString();
